5862e8db92e464fcd1bf603b2afb24a37fc597ea
Tracy Hocutt <tracy@xsyte.com>
2019-08-20 16:36:22 +1000

    Updates

---
+++ precache-manifest.53829b7f86d198da00a65b5d73903c38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d014335aaa9f6a86c206b6c44ab3f1ed",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fab45cb81f891ac19c62",
+    "url": "/monsters-rolodex/static/css/main.db3bf324.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "fab45cb81f891ac19c62",
+    "url": "/monsters-rolodex/static/js/main.0ca6ddf1.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
